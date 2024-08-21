bc5efb3c60c0664a6405837fca520611b162f01f
Ximena Capera <ximenacapera79@gmail.com>
2024-08-20 20:42:46 -0500

    cambiom 6

--- src/calculadora.py
+++ src/calculadora.py
@@ -13,12 +13,12 @@ def calcular(numero1, numero2, operacion):
         case '/':
             # Operación división
             respuesta = numero1 / numero2
-        case '_':
-            raise ValueError("Operación inválida.")
         case '%':
             respuesta = numero1 % numero2
         case '^':
             respuesta = numero1 ** numero2
+        case _: 
+            raise ValueError("Operación inválida.")
 
         # TODO: DEBES COLOCAR TU CÓDIGO AQUÍ# TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO, COMPROBANDO LOS SÍMBOLOS ^ Y %.
 
